8257d3151a624833cc6d299339d80812155bc5e3
MattiaDaddazio <49347654+MattiaDaddazio@users.noreply.github.com>
2020-04-16 15:37:29 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,30 @@
+let css = document.querySelector("h3");
+let color1 = document.querySelector(".color1");
+let color2 = document.querySelector(".color2");
+let body = document.getElementById("gradient");
+let button = document.querySelector("button");
+
+function randomGradient() {
+	color1.value = '#'+Math.floor(Math.random()*16777215).toString(16);
+	color2.value = '#'+Math.floor(Math.random()*16777215).toString(16);
+	setGradient();
+}
+
+function setGradient() {
+	body.style.background = 
+	"linear-gradient(to right, " 
+	+ color1.value 
+	+ ", " 
+	+ color2.value 
+	+ ")";
+
+	css.textContent = body.style.background + ";";
+}
+
+color1.addEventListener("input", setGradient);
+
+color2.addEventListener("input", setGradient);
+
+button.addEventListener("click", randomGradient);
+
+setGradient();
